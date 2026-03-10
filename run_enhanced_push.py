8e87e24b5879c5070d246bf05df780994adabaff
SINGHEIL <165178188+SINGHEIL@users.noreply.github.com>
2026-03-10 22:53:07 +0800

    run_enhanced_push.py
    添加增强版运行脚本

---
+++ run_enhanced_push.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+try:
+    from enhanced_news_fetcher import EnhancedNewsFetcher
+    from enhanced_report_generator import EnhancedReportGenerator
+    from wechat_notifier import WeChatNotifier
+    
+    logging.info('开始抓取新闻...')
+    fetcher = EnhancedNewsFetcher()
+    
+    logging.info('抓取所有新闻源...')
+    all_news = fetcher.fetch_all_news()
+    
+    logging.info('生成简报...')
+    generator = EnhancedReportGenerator()
+    report = generator.filter_and_format(all_news)
+    
+    logging.info('初始化微信推送...')
+    import os
+    notifier_config = {
+        'WECHAT_TEST': True,
+        'WECHAT_TEST_APPID': os.getenv('WECHAT_TEST_APPID', ''),
+        'WECHAT_TEST_SECRET': os.getenv('WECHAT_TEST_SECRET', ''),
+        'WECHAT_TEST_TOUSER': os.getenv('WECHAT_TEST_TOUSER', ''),
+    }
+    notifier = WeChatNotifier(notifier_config)
+    
+    logging.info('推送到微信...')
+    success = notifier.format_and_send(report)
+    
+    if success:
+        logging.info('推送成功!')
+        sys.exit(0)
+    else:
+        logging.error('推送失败!')
+        sys.exit(1)
+        
+except Exception as e:
+    logging.error(f'程序异常: {str(e)}', exc_info=True)
+    sys.exit(1)
